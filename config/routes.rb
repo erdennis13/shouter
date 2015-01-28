776204c47e285e05d09bca825142cf341c09df59
Ethan Dennis <erdennis13@gmail.com>
2015-01-28 08:16:31 -0800

    Added dashboard functionality, unfollow user functionality, and refractoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: "homes#show", via: :get
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show] do
+  resources :users, only: [:new, :create, :show, :index] do
   	post 'follow' => 'following_relationships#create'
+  	delete 'follow' => "following_relationships#destroy"
   end
   resource :dashboard, only: :show
   resources :shouts, only: [:show]
