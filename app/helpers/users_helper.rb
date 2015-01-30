166676479e6443ecb1e9fe9fe61cf82498635df8
Ethan Dennis <erdennis13@gmail.com>
2015-01-30 15:22:47 -0800

    Shouter app (twitter clone)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
 	def follow_button user
 		if current_user.following? user
-			button_to "Unfollow", user_follow_path(user), method: :delete
+			button_to "Unfollow", user_follow_path(user), method: :delete, class: "btn btn-primary btn-sm"
 		else	
-			button_to 'Follow', user_follow_path(user)
+			button_to 'Follow', user_follow_path(user), class: "btn btn-primary btn-sm"
 		end
 	end
 end
